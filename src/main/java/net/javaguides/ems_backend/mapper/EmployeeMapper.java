a7069c8fae556e694dbf20551b7b67c0be8f5476
SAIROHITH AMBATI <sairohith10@outlook.com>
2024-11-13 20:29:14 +0530

    second commit

---
+++ src/main/java/net/javaguides/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package net.javaguides.ems_backend.mapper;
+
+import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto maptoEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee maptoEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+
+}
